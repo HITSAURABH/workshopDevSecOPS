6e29a73df43cecc9f5df0e8d2d6bba88fbbfce68
Raushan  Raj <raushanraj@Raushans-MacBook-Air-2.local>
2025-02-22 05:32:26 +0530

    ookk

--- leaks.py
+++ leaks.py
@@ -2,7 +2,7 @@ AWS_ACCESS_KEY_ID=AKIAFAKE123456789
 AWS_SECRET_ACCESS_KEY=abcd1234fakeSecretKeyTest
 
 #ghp_f4k3G1thubP4TTestToken123456
-
+#https://hooks.slack.com/services/SDAJNF45/DS5G45RSD/uighjbbjkoahndf5S4G5515D1
 
 
 def get_s3_buckets():
